b0a1f55e7815286326649654aa8caae7dc4d9ec5
Josianne <anne.jmfreitas@gmail.com>
2024-12-27 00:32:53 -0400

    :bookmark: Release

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import './App.css'
+import qrCode from './assets/qr.svg'
 
 export default function App() {
   return (
-    <h1>Hello World!</h1>
+    <main className="bg-white w-80 rounded-2xl flex flex-col px-4 pt-4 pb-10 gap-6 shadow-shadow">
+      <div className="bg-blue-600 rounded-xl p-16">
+        <img src={qrCode} alt="QR Code" />
+      </div>
+      <div className="font-serif antialiased text-center flex flex-col px-4 gap-4">
+        <h1 className="text-xl font-bold leading-relaxed text-slate-900">
+          Improve your front-end skills by building projects
+        </h1>
+        <p className="text-sm font-normal tracking-wide leading-snug text-slate-500">
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
+      </div>
+    </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      blue: {
+        600: '#2C7DFA'
+      },
+      slate: {
+        900: '#1f314f',
+        500: '#68778d',
+        300: '#d5e1ef'
+      },
+      white: '#ffffff'
+    },
+    fontFamily: {
+      serif: ['Outfit', 'serif']
+    },
+    extend: {
+      boxShadow: {
+        shadow: '0 25px 25px 0 rgba(0, 0, 0, 0.05)'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
